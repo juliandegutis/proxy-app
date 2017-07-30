81fb7229689b5baf3a7e242b75d2b93e697ec81a
juliandegutis <jdegutis87@gmail.com>
2017-07-30 20:55:03 -0300

    Add files via upload

---
+++ src/main/java/br/com/proxyapp/ProxyAppApplication.java
@@ -0,0 +1,12 @@
+package br.com.proxyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyAppApplication.class, args);
+	}
+}
---
+++ src/main/java/br/com/proxyapp/proxy/ProxyController.java
@@ -0,0 +1,20 @@
+package br.com.proxyapp.proxy;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping( "/" )
+public class ProxyController {
+	
+//	@RequestMapping( value = "", method = RequestMethod.GET )
+//	public void get( HttpServletRequest request, HttpServletResponse response ) {
+//		System.out.println("GET");
+//	}
+//	
+//	@RequestMapping( value = "", method = RequestMethod.POST )
+//	public void post( HttpServletRequest request, HttpServletResponse response ) {
+//		System.out.println("POST");
+//	}
+	
+}

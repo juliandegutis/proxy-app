0498284060d4fb3097309f6abd33b044d10f46ea
Julian Degutis de Freitas Garcia <juliang@kyros.com.br>
2017-07-31 00:47:02 -0300

    2

--- src/main/java/br/com/proxyapp/ProxyAppApplication.java
+++ src/main/java/br/com/proxyapp/ProxyAppApplication.java
@@ -1,9 +1,17 @@
 package br.com.proxyapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableWebMvc
+@ComponentScan
 public class ProxyAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/proxyapp/controller/ProxyController.java
+++ src/main/java/br/com/proxyapp/controller/ProxyController.java
@@ -2,23 +2,20 @@ package br.com.proxyapp.controller;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.net.URLDecoder;
-import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.com.proxyapp.enums.HttpHeaderEnum;
 
 @RestController
 @RequestMapping( "/" )
@@ -29,94 +26,79 @@ public class ProxyController {
 
 		try {
 
-			Enumeration< String > headerNames = request.getHeaderNames();
-
-			while ( headerNames.hasMoreElements() ) {
-
-				String headerName = headerNames.nextElement();
-
-				System.out.println( headerName + " : " + request.getHeader( headerName ) );
-
-			}
-
 			OutputStreamWriter writer = new OutputStreamWriter( response.getOutputStream() );
-			HttpClient client = new HttpClient();
 
-			HttpMethod method = null;
+			HttpClient httpClient = HttpClientBuilder.create().build();
 
-			method = new GetMethod( URLDecoder.decode( request.getHeader( HttpHeaderEnum.URL.value() ), "utf-8" ) );
+			HttpGet requesth = new HttpGet( "http://192.168.1.5:8080/greeting" );
+			ResponseHandler< String > responseHandler = new BasicResponseHandler();
 
-			client.executeMethod( method );
-
-			Header[] headers = method.getResponseHeaders();
-			for ( Header header : headers ) {
-
-				if ( "Content-Type".equalsIgnoreCase( header.getName() ) ) {
-
-					response.setContentType( header.getValue() );
-				}
-			}
-
-			writer.write( method.getResponseBodyAsString() );
+	        HttpContext context = new BasicHttpContext(); 
+	        String responseH = httpClient.execute(requesth, responseHandler, context);
+			
+	        writer.write( responseH );
 			writer.flush();
 			writer.close();
 
-		} catch ( HttpException e ) {
-			e.printStackTrace();
 		} catch ( IOException e ) {
 			e.printStackTrace();
 		}
 	}
 
-	@RequestMapping( value = "", method = RequestMethod.POST )
-	public void post( HttpServletRequest request, HttpServletResponse response ) {
-		try {
-
-			Enumeration< String > headerNames = request.getHeaderNames();
-
-			while ( headerNames.hasMoreElements() ) {
-
-				String headerName = headerNames.nextElement();
-
-				System.out.println( headerName + " : " + request.getHeader( headerName ) );
-
-			}
-
-			OutputStreamWriter writer = new OutputStreamWriter( response.getOutputStream() );
-			HttpClient client = new HttpClient();
-
-			HttpMethod method = null;
-
-			method = new PostMethod( URLDecoder.decode( request.getHeader( HttpHeaderEnum.URL.value() ), "utf-8" ) );
-
-			Enumeration< String > paramNames = request.getParameterNames();
-
-			while ( paramNames.hasMoreElements() ) {
-
-				String paramName = paramNames.nextElement();
-				( (PostMethod) method ).setParameter( paramName, request.getParameter( paramName ) );
-			}
-
-			client.executeMethod( method );
-
-			Header[] headers = method.getResponseHeaders();
-			for ( Header header : headers ) {
-
-				if ( "Content-Type".equalsIgnoreCase( header.getName() ) ) {
-
-					response.setContentType( header.getValue() );
-				}
-			}
-
-			writer.write( method.getResponseBodyAsString() );
-			writer.flush();
-			writer.close();
-
-		} catch ( HttpException e ) {
-			e.printStackTrace();
-		} catch ( IOException e ) {
-			e.printStackTrace();
-		}
-	}
+	// @RequestMapping( value = "", method = RequestMethod.POST )
+	// public void post( HttpServletRequest request, HttpServletResponse
+	// response ) {
+	// try {
+	//
+	// Enumeration< String > headerNames = request.getHeaderNames();
+	//
+	// while ( headerNames.hasMoreElements() ) {
+	//
+	// String headerName = headerNames.nextElement();
+	//
+	// System.out.println( headerName + " : " + request.getHeader( headerName )
+	// );
+	//
+	// }
+	//
+	// OutputStreamWriter writer = new OutputStreamWriter(
+	// response.getOutputStream() );
+	// HttpClient client = new HttpClient();
+	//
+	// HttpMethod method = null;
+	//
+	// method = new PostMethod( URLDecoder.decode( request.getHeader(
+	// HttpHeaderEnum.URL.value() ), "utf-8" ) );
+	//
+	// Enumeration< String > paramNames = request.getParameterNames();
+	//
+	// while ( paramNames.hasMoreElements() ) {
+	//
+	// String paramName = paramNames.nextElement();
+	// ( (PostMethod) method ).setParameter( paramName, request.getParameter(
+	// paramName ) );
+	// }
+	//
+	// client.executeMethod( method );
+	//
+	// Header[] headers = method.getResponseHeaders();
+	// for ( Header header : headers ) {
+	//
+	// if ( "Content-Type".equalsIgnoreCase( header.getName() ) ) {
+	//
+	// response.setContentType( header.getValue() );
+	// }
+	// }
+	//
+	// writer.write( method.getResponseBodyAsString() );
+	// writer.flush();
+	// writer.close();
+	//
+	// } catch ( HttpException e ) {
+	// e.printStackTrace();
+	// } catch ( IOException e ) {
+	// e.printStackTrace();
+	// }
+	// }
 
 }
